23ed00aab0be6a895f6c686822d31ad376be509f
tjespe <tordjohanespe@gmail.com>
2017-10-28 14:12:02 +0200

    Added injection script

---
+++ block.js
@@ -0,0 +1,25 @@
+console.log("Running block.js", location);
+
+/**
+ * Gets the saved state for url.
+ *
+ * @param {string} url URL whose background color is to be retrieved.
+ * @param {function(string)} callback called with the saved state value for
+ *     the given url on success, or a false value if no state is retrieved.
+ */
+function getSavedPageState(url, callback) {
+  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
+  // for chrome.runtime.lastError to ensure correctness even when the API call
+  // fails.
+  chrome.storage.sync.get(url, (items) => {
+    callback(chrome.runtime.lastError ? false : items[url]);
+  });
+}
+
+/* Prompt user for password */
+getSavedPageState(location.hostname, (state)=>{
+  console.log(state);
+  if (state) {
+    prompt("Please enter your password");
+  }
+});
