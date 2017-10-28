79a5d639c6be8a0f87a412fcb8c977f14c24396e
tjespe <tordjohanespe@gmail.com>
2017-10-28 18:17:02 +0200

    Added 15 minutes password free after typing password

--- block.js
+++ block.js
@@ -20,20 +20,28 @@ function getSavedPageState(url, callback) {
 getSavedPageState(location.hostname, (state)=>{
   console.log(state);
   if (state) {
-    document.body.innerHTML += `<div id="overlay-pr" style="
+    chrome.storage.sync.get("last_auth", (items)=>{
+      if (typeof items.last_auth === "undefined" || Date.now()-items.last_auth > 1000*60*15) {
+        document.body.innerHTML += `<div id="overlay-pr" style="
         height: 100%;
         width: 100%;
         position: absolute;
         background: #fff;
         z-index: 99999;
-      top: 0;
-    "></div>`
-    chrome.storage.sync.get("password", (items)=>{
-      while (prompt("Please enter your password") !== items.password) {
-        alert("Sorry! Wrong password! Please try again!");
+        top: 0;
+        "></div>`
+        chrome.storage.sync.get("password", (items)=>{
+          while (prompt("Please enter your password") !== items.password) {
+            alert("Sorry! Wrong password! Please try again!");
+          }
+          console.log("Passwords matched!");
+          document.body.removeChild(document.getElementById("overlay-pr"));
+          var items = {};
+          items.last_auth = Date.now();
+          chrome.storage.sync.set(items);
+          if (typeof callback !== 'undefined') callback(!state);
+        });
       }
-      console.log("Passwords matched!");
-      document.body.removeChild(document.getElementById("overlay-pr"));
     });
   }
 });
