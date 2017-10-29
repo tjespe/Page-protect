0d0445e5a12bcdad8aa8547a8d58de5bb901b30a
tjespe <tordjohanespe@gmail.com>
2017-10-29 19:10:58 +0100

    Improved password free period system

--- block.js
+++ block.js
@@ -20,28 +20,24 @@ function getSavedPageState(url, callback) {
 getSavedPageState(location.hostname, (state)=>{
   console.log(state);
   if (state) {
-    chrome.storage.sync.get("last_auth", (items)=>{
-      if (typeof items.last_auth === "undefined" || Date.now()-items.last_auth > 1000*60*15) {
-        document.body.innerHTML += `<div id="overlay-pr" style="
-        height: 100%;
-        width: 100%;
-        position: absolute;
-        background: #fff;
-        z-index: 99999;
-        top: 0;
-        "></div>`
-        chrome.storage.sync.get("password", (items)=>{
-          while (prompt("Please enter your password") !== items.password) {
-            alert("Sorry! Wrong password! Please try again!");
-          }
-          console.log("Passwords matched!");
-          document.body.removeChild(document.getElementById("overlay-pr"));
-          var items = {};
-          items.last_auth = Date.now();
-          chrome.storage.sync.set(items);
-          if (typeof callback !== 'undefined') callback(!state);
-        });
-      }
-    });
+    if (typeof sessionStorage.last_auth === "undefined" || Date.now()-sessionStorage.last_auth > 1000*60*15) {
+      document.body.innerHTML += `<div id="overlay-pr" style="
+      height: 100%;
+      width: 100%;
+      position: absolute;
+      background: #fff;
+      z-index: 99999;
+      top: 0;
+      "></div>`
+      chrome.storage.sync.get("password", (items)=>{
+        while (prompt("Please enter your password") !== items.password) {
+          alert("Sorry! Wrong password! Please try again!");
+        }
+        console.log("Passwords matched!");
+        document.body.removeChild(document.getElementById("overlay-pr"));
+        sessionStorage.last_auth = Date.now();
+        if (typeof callback !== 'undefined') callback(!state);
+      });
+    }
   }
 });
